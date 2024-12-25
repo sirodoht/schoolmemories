c16df85a39c001c83327bd224eb3392eba1510df
Theodore Keloglou <zf@sirodoht.com>
2024-12-25 00:19:57 +0000

    Add custom domain and body as html on models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import mistune
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -17,6 +18,12 @@ class User(AbstractUser):
     )
     email = models.EmailField(unique=True)
     website_title = models.CharField(max_length=500, blank=True, null=True)
+    custom_domain = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        validators=[validators.validate_domain_name],
+    )
 
     def __str__(self):
         return self.username
@@ -33,6 +40,11 @@ class Page(models.Model):
     title = models.CharField(max_length=300)
     body = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def body_as_html(self):
+        markdown = mistune.create_markdown(plugins=["task_lists", "footnotes"])
+        return markdown(self.body)
 
     def __str__(self):
         return self.title
