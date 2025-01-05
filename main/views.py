3eb9a75e1ed631f181240aa26b77833a71600445
Theodore Keloglou <zf@sirodoht.com>
2025-01-05 16:56:23 +0000

    Add image CRUD view methods

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 
 from django.conf import settings
 from django.contrib import messages
@@ -8,13 +9,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LogoutView as DjLogoutView
 from django.core.exceptions import PermissionDenied
 from django.http import (
+    Http404,
     HttpResponse,
+    HttpResponseBadRequest,
     HttpResponseRedirect,
 )
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
 
 from main import forms, models
 
@@ -114,6 +117,13 @@ def domain_check(request):
     raise PermissionDenied()
 
 
+def markdown(request):
+    return render(request, "main/markdown.html")
+
+
+# Users and user settings
+
+
 class UserCreate(CreateView):
     form_class = forms.UserCreationForm
     success_message = "welcome :)"
@@ -153,6 +163,16 @@ class UserUpdate(LoginRequiredMixin, UpdateView):
         return form
 
 
+class CSSUpdate(LoginRequiredMixin, UpdateView):
+    model = models.User
+    fields = ["custom_css"]
+    template_name = "main/custom_css.html"
+    success_url = reverse_lazy("css_update")
+
+    def get_object(self):
+        return self.request.user
+
+
 @login_required
 def dashboard(request):
     if hasattr(request, "subdomain"):
@@ -168,17 +188,22 @@ def dashboard(request):
     )
 
 
+# Pages
+
+
 class PageCreate(LoginRequiredMixin, CreateView):
     model = models.Page
     fields = ["title", "slug", "body"]
     template_name = "main/page_create.html"
-    success_url = reverse_lazy("index")
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse("page_detail", args=(self.object.slug,))
 
 
 class PageDetail(DetailView):
@@ -200,6 +225,18 @@ class PageDetail(DetailView):
             )
 
         return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if hasattr(request, "subdomain"):
+            return super().dispatch(request, *args, **kwargs)
+
+        if request.user.is_authenticated:
+            subdomain = request.user.username
+            return redirect(
+                f"{settings.PROTOCOL}//{subdomain}.{settings.CANONICAL_HOST}{request.path}"
+            )
+        else:
+            return redirect("index")
 
 
 class PageUpdate(LoginRequiredMixin, UpdateView):
@@ -246,11 +283,110 @@ class PageDelete(LoginRequiredMixin, DeleteView):
         return super().dispatch(request, *args, **kwargs)
 
 
-class CSSUpdate(LoginRequiredMixin, UpdateView):
-    model = models.User
-    fields = ["custom_css"]
-    template_name = "main/custom_css.html"
-    success_url = reverse_lazy("css_update")
+# Images
 
-    def get_object(self):
-        return self.request.user
+
+class ImageList(LoginRequiredMixin, FormView):
+    form_class = forms.UploadImagesForm
+    template_name = "main/image_list.html"
+    success_url = reverse_lazy("image_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["image_list"] = models.Image.objects.filter(user=self.request.user)
+
+        context["total_quota"] = 0
+        for image in models.Image.objects.filter(user=self.request.user):
+            context["total_quota"] += image.data_size
+        context["total_quota"] = round(context["total_quota"], 2)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        files = request.FILES.getlist("file")
+        if form.is_valid():
+            for f in files:
+                name_ext_parts = f.name.rsplit(".", 1)
+                name = name_ext_parts[0].replace(".", "-")
+                self.extension = name_ext_parts[1].casefold()
+                if self.extension == "jpg":
+                    self.extension = "jpeg"
+                data = f.read()
+
+                # Image file limit 1.1MB = 1.1 * 1000^2
+                if len(data) > 1.1 * 1000 * 1000:
+                    form.add_error("file", "File too big. Limit is 1MB.")
+                    return self.form_invalid(form)
+
+                self.slug = str(uuid.uuid4())[:8]
+                models.Image.objects.create(
+                    name=name,
+                    data=data,
+                    extension=self.extension,
+                    user=request.user,
+                    slug=self.slug,
+                )
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_success_url(self):
+        # if ?raw=true in url, return to image_raw instead of image_list
+        if (
+            len(self.request.FILES.getlist("file")) == 1
+            and self.request.GET.get("raw") == "true"
+        ):
+            return reverse("image_raw", args=(self.slug, self.extension))
+        else:
+            return str(self.success_url)  # success_url is lazy
+
+    def form_invalid(self, form):
+        # if ?raw=true in url, return form error as string
+        if (
+            len(self.request.FILES.getlist("file")) == 1
+            and self.request.GET.get("raw") == "true"
+        ):
+            return HttpResponseBadRequest(" ".join(form.errors["file"]))
+        else:
+            return super().form_invalid(form)
+
+
+class ImageDetail(LoginRequiredMixin, DetailView):
+    model = models.Image
+
+    def dispatch(self, request, *args, **kwargs):
+        image = self.get_object()
+        if request.user != image.user:
+            raise PermissionDenied()
+        return super().dispatch(request, *args, **kwargs)
+
+
+async def image_raw(request, slug, extension):
+    image = await models.Image.objects.filter(slug=slug).afirst()
+    if not image or extension != image.extension:
+        raise Http404()
+    return HttpResponse(image.data, content_type="image/" + image.extension)
+
+
+class ImageUpdate(LoginRequiredMixin, UpdateView):
+    model = models.Image
+    fields = ["name"]
+    template_name = "main/image_edit.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        image = self.get_object()
+        if request.user != image.user:
+            raise PermissionDenied()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ImageDelete(LoginRequiredMixin, DeleteView):
+    model = models.Image
+    success_url = reverse_lazy("image_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        image = self.get_object()
+        if request.user != image.user:
+            raise PermissionDenied()
+        return super().dispatch(request, *args, **kwargs)
