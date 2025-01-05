daa05f7a756ba14d400c500d7765443285aa5707
Theodore Keloglou <zf@sirodoht.com>
2025-01-05 16:55:09 +0000

    Add image upload on page edit and create templates

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm as DjUserCreationForm
+from django.core import validators as dj_validators
 
 from main import models
 
@@ -21,3 +22,29 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ["username", "email", "website_title", "custom_domain", "home"]
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, list | tuple):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
+class UploadImagesForm(forms.Form):
+    file = MultipleFileField(
+        validators=[
+            dj_validators.FileExtensionValidator(["jpeg", "jpg", "png", "gif", "webp"])
+        ],
+    )
