50da276009e78e561c67dc65fe9dc2bbde00a1f6
Theodore Keloglou <zf@sirodoht.com>
2025-04-12 19:43:20 +0100

    Rename django project to schoolmemories

--- schoolmemories/asgi.py
+++ schoolmemories/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dukkha project.
+ASGI config for schoolmemories project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukkha.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolmemories.settings")
 
 application = get_asgi_application()
--- schoolmemories/wsgi.py
+++ schoolmemories/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dukkha project.
+WSGI config for schoolmemories project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukkha.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolmemories.settings")
 
 application = get_wsgi_application()
